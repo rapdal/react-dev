fc42370446b361422611f4a7e7442b306fa85448
rad <rad>
2017-02-07 14:18:44 +1100

    first commit

---
+++ webpack.dev.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'eval',
+  
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/main.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/public/'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  module: {
+    loaders: [
+      { 
+        test: /\.js[x]?$/,
+        loader: 'babel-loader',
+        query: {
+           presets: ['es2015', 'react']
+        },
+        exclude: path.join(__dirname, 'node_modules') 
+      },
+      { test: /\.scss?$/,
+        loader: 'style!css!sass',
+        include: path.join(__dirname, 'src', 'styles') },
+      { test: /\.png$/,
+        loader: 'file' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file'}
+    ]
+  }
+}
---
+++ webpack.prod.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'source-map',
+
+  entry: [
+    './src/main.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/public/'
+  },
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+
+  module: {
+    loaders: [
+      { 
+        test: /\.js[x]?$/,
+        loader: 'babel-loader',
+        query: {
+           presets: ['es2015', 'react']
+        },
+        exclude: path.join(__dirname, 'node_modules') 
+      },
+      { test: /\.scss?$/,
+        loader: 'style!css!sass',
+        include: path.join(__dirname, 'src', 'styles') },
+      { test: /\.png$/,
+        loader: 'file' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file'}
+    ]
+  }
+}
